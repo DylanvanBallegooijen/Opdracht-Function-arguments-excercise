56b3793bc9ca8b6700c44aa20609454b18d86c8c
DylanvanBallegooijen <78920682+DylanvanBallegooijen@users.noreply.github.com>
2021-07-19 10:24:59 +0200

    Add files via upload

---
+++ Opdracht Function arguments excercise/script.js
@@ -0,0 +1,15 @@
+const Bansky = function (item) {
+    console.log('The wall has been painted ' + item);
+};
+
+
+const main_paint = 'red';
+const all_over = 'everywhere';
+
+
+Bansky("paint");
+Bansky('wall');
+Bansky('roller');
+
+Bansky(main_paint);
+Bansky(all_over);
